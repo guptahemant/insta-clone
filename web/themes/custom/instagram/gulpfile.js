a7ff5cbe66c968bc8c45f7bd20a3b9bc01b0343c
atulghate <atulghate155.com>
2021-12-15 17:00:20 +0530

    created slider for login page

--- web/themes/custom/instagram/gulpfile.js
+++ web/themes/custom/instagram/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 gulp.task('sass', function() {
-    return gulp.src('scss/*/style.scss')
+    return gulp.src('scss/*/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css/'));
 });
