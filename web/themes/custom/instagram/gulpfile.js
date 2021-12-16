b0cac65154d472925eedaf789a6015a5f15b8784
atulghate <atulghate155.com>
2021-12-16 21:41:14 +0530

    added gulp task for minify css and js and watch task

--- web/themes/custom/instagram/gulpfile.js
+++ web/themes/custom/instagram/gulpfile.js
@@ -1,7 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const minify= require('gulp-minify');
+var uglifycss = require('gulp-uglifycss');
 gulp.task('sass', function() {
-    return gulp.src('scss/*/style.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css/'));
-});
+        return gulp.src('scss/*/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('css/'));
+    });
+    gulp.task('css', async function () {
+        gulp.src('css/*/*.css')
+          .pipe(uglifycss({
+            "maxLineLen": 80,
+            "uglyComments": true
+          }))
+          .pipe(gulp.dest('./css/'));
+      });
+      gulp.task('compress', async function() {
+        gulp.src(['js/instagram.js'])
+          .pipe(minify())
+          .pipe(gulp.dest('js'))
+      });
+      gulp.task('watch', function() {
+            gulp.watch(['scss/all/*.scss'],gulp.series('sass'));
+          });
