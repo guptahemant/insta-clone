dcaa7595678f28902ac43767b5f61f2e6b9f824e
pritamjadhav <88623137+pritamyj@users.noreply.github.com>
2022-01-03 03:07:49 +0530

    added myprofile block

--- web/themes/custom/instagram/gulpfile.js
+++ web/themes/custom/instagram/gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('scss/*/style.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css/'));
- 
+    return gulp.src('scss/style.scss')
+        .pipe(sass()) 
+        .pipe(gulp.dest('css/all'));
 });
 
 gulp.task('default', gulp.series('sass')); 
 
 gulp.task('watch', function(){ 
-   gulp.watch('scss/*',gulp.series('sass'));
+ 
+   gulp.watch('scss/*/*',gulp.series('sass'));
  
 });
