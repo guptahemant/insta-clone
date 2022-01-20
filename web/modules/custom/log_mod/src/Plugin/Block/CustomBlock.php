b451be5870d9cc5e91d94098080adedbd8b3a857
sharayu0 <sharayurajput1998@gmail.com>
2022-01-20 19:53:35 +0530

    committing the code of slider and login form in 2-login

---
+++ web/modules/custom/log_mod/src/Plugin/Block/CustomBlock.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\log_mod\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "custom_block",
+ *   admin_label = @Translation("Custom Block"),
+ *   category = @Translation("Hello World"),
+ * )
+ */
+class CustomBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+      
+    $out = 'Hello, World!';
+    dump($out);
+     
+    return [
+      '#theme' => 'cust-block',
+      '#data' =>  $out,
+      
+    ];
+  }
+
+}
