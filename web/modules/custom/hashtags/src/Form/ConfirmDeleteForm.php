f9a36165c88f063dc8ca3beb2be6762cea97c123
vishalbandre <iamvshlbndr@gmail.com>
2022-03-17 17:12:40 +0530

    #91 Hashtags Implementation + Coding Standards Fixed

--- web/modules/custom/hashtags/src/Form/ConfirmDeleteForm.php
+++ web/modules/custom/hashtags/src/Form/ConfirmDeleteForm.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 24.12.2018
- * Time: 15:55
- */
 
 namespace Drupal\hashtags\Form;
 
@@ -13,91 +7,112 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\Core\Url;
 
+/**
+ * {@inheritdoc}
+ */
 class ConfirmDeleteForm extends ConfirmFormBase {
-    private $entity_type;
-    private $bundle;
+  /**
+   * The Entity type property.
+   *
+   * @var string
+   */
+  private $entityType;
 
-    /**
-     * Returns the question to ask the user.
-     * @return \Drupal\Core\StringTranslation\TranslatableMarkup
-     *   The form question. The page title will be set to this value.
-     */
-    public function getQuestion() {
-        $entity_type_label = _hashtags_get_entity_type_label($this->entity_type);
-        $bundle_label = _hashtags_get_bundle_label($this->entity_type,$this->bundle);
-        $source = $this->entity_type !== $this->bundle ?
+  /**
+   * The Entity bundle property.
+   *
+   * @var string
+   */
+  private $bundle;
+
+  /**
+   * Returns the question to ask the user.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   The form question. The page title will be set to this value.
+   */
+  public function getQuestion() {
+    $entity_type_label = _hashtags_get_entity_type_label($this->entityType);
+    $bundle_label = _hashtags_get_bundle_label($this->entityType, $this->bundle);
+    $source = $this->entityType !== $this->bundle ?
                   ($entity_type_label . ' > ' . $bundle_label) :
                   $entity_type_label;
-        return $this->t("Are you sure you want to remove hashtags for <em>{$source}</em>?");
-    }
+    return $this->t("Are you sure you want to remove hashtags for <em>@source</em>?", ['@source' => $source]);
+  }
 
-    /**
-     * Returns the route to go to if the user cancels the action.
-     * @return \Drupal\Core\Url
-     *   A URL object.
-     */
-    public function getCancelUrl() {
-        return new Url('hashtags.manager_form');
-    }
+  /**
+   * Returns the route to go to if the user cancels the action.
+   *
+   * @return \Drupal\Core\Url
+   *   A URL object.
+   */
+  public function getCancelUrl() {
+    return new Url('hashtags.manager_form');
+  }
 
-    /**
-     *
-     * @return \Drupal\Core\StringTranslation\TranslatableMarkup
-     */
-    public function getConfirmText() {
-        return $this->t('Delete');
-    }
+  /**
+   * {@inheritdoc}
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   Return translated string.
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
 
-    /**
-     * Returns a unique string identifying the form.
-     * The returned ID should be a unique string that can be a valid PHP function
-     * name, since it's used in hook implementation names such as
-     * hook_form_FORM_ID_alter().
-     * @return string
-     *   The unique string identifying the form.
-     */
-    public function getFormId() {
-        return 'hashtags_delete_form';
-    }
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'hashtags_delete_form';
+  }
 
-    public function buildForm(array $form,
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form,
                               FormStateInterface $form_state,
-                              $entity_type = '', $bundle = '') {
-        $this->entity_type = $entity_type;
-        $this->bundle = $bundle;
+                              $entity_type = '',
+  $bundle = '') {
+    $this->entityType = $entity_type;
+    $this->bundle = $bundle;
 
-        return parent::buildForm($form, $form_state);
-    }
+    return parent::buildForm($form, $form_state);
+  }
 
-
-    /**
-     * Form submission handler.
-     * @param array $form
-     *   An associative array containing the structure of the form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *   The current state of the form.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        $hashtags_field_name = \Drupal::config('hashtags.settings')
-                                ->get('hashtags_taxonomy_terms_field_name');
-        $hashtags_field = FieldConfig::loadByName($this->entity_type,
-                                         $this->bundle,
-                                         $hashtags_field_name);
-        if (!empty($hashtags_field)) {
-            $hashtags_field->delete();
-            \Drupal::messenger()->addMessage('Hashtags field has been removed.');
-            $activated_text_fields = _hashtags_get_activated_text_fields($this->entity_type, $this->bundle);
-            foreach ($activated_text_fields as $field_name) {
-                $text_field = \Drupal::entityTypeManager()
-                    ->getStorage('field_config')
-                    ->load("{$this->entity_type}.{$this->bundle}.{$field_name}");
-                if (!empty($text_field)) {
-                    $text_field->unsetThirdPartySetting('hashtags', 'hashtags_activate');
-                    $text_field->save();
-                    \Drupal::messenger()->addMessage("Hashtags has been diactivated for {$field_name} field.");
-                }
-            }
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $hashtags_field_name = \Drupal::config('hashtags.settings')
+      ->get('hashtags_taxonomy_terms_field_name');
+    $hashtags_field = FieldConfig::loadByName($this->entityType,
+                                       $this->bundle,
+                                       $hashtags_field_name);
+    if (!empty($hashtags_field)) {
+      $hashtags_field->delete();
+      \Drupal::messenger()->addMessage('Hashtags field has been removed.');
+      $activated_text_fields = _hashtags_get_activated_text_fields($this->entityType, $this->bundle);
+      foreach ($activated_text_fields as $field_name) {
+        $text_field = \Drupal::entityTypeManager()
+          ->getStorage('field_config')
+          ->load("{$this->entityType}.{$this->bundle}.{$field_name}");
+        if (!empty($text_field)) {
+          $text_field->unsetThirdPartySetting('hashtags', 'hashtags_activate');
+          $text_field->save();
+          \Drupal::messenger()->addMessage("Hashtags has been diactivated for {$field_name} field.");
         }
-        $form_state->setRedirectUrl(new Url('hashtags.manager_form'));
+      }
     }
+    $form_state->setRedirectUrl(new Url('hashtags.manager_form'));
+  }
+
 }
--- web/modules/custom/hashtags/src/Plugin/Filter/LinksConverter.php
+++ web/modules/custom/hashtags/src/Plugin/Filter/LinksConverter.php
@@ -1,48 +1,58 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 20.12.2018
- * Time: 22:28
- */
 
 namespace Drupal\hashtags\Plugin\Filter;
+
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 
-/*
- * Helper class to pass parameters to callback function within preg_replace_callback
+/**
+ * Helper class to parameters to callback function within preg_replace_callback.
  */
 class LinksConverter {
-    private $hashtags_tids;
+  /**
+   * Hashtag Ids property.
+   *
+   * @var array
+   */
+  private $hashtagsTids;
 
-    function __construct($hashtags_tids) {
-        $this->hashtags_tids = $hashtags_tids;
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($hashtagsTids) {
+    $this->hashtagsTids = $hashtagsTids;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function replace($matches) {
+    if (isset($this->hashtagsTids)) {
+      $hashtagsTids = $this->hashtagsTids;
     }
-
-    function replace($matches) {
-        if (isset($this->hashtags_tids)) {
-            $hashtags_tids = $this->hashtags_tids;
-        }
-        $first_delimeter = isset($matches[1]) ? $matches[1] : '';
-        $hashtag_name = isset($matches[3]) ? $matches[3] : '';
-        $hashtag_tid = isset($this->hashtags_tids[strtolower($hashtag_name)]) ? $this->hashtags_tids[strtolower($hashtag_name)] : '';
-        $hashtag_name = '#'.$hashtag_name;
-        // hashtag is not exists - show without link
-        if (empty($hashtag_tid)) {
-            return $first_delimeter . $hashtag_name;
-        }
-        // Fatal error: [] operator not supported for strings in /includes/common.inc on line 2442
-        // Issue comes up when we try to bind attribute to link which has path parameter of the current page............
-        /*if ($_GET['q'] == 'taxonomy/term/'.$hashtag_tid) {
-            $hashtag_link = l($hashtag_name, 'taxonomy/term/'.$hashtag_tid);
-        } else {
-            $hashtag_link = l($hashtag_name, 'taxonomy/term/'.$hashtag_tid, array('attributes' => array('class' => 'hashtag')));
-        }*/
-        $hashtag_link = Link::fromTextAndUrl($hashtag_name,
-            Url::fromRoute('entity.taxonomy_term.canonical', array('taxonomy_term' => $hashtag_tid),
-                array('attributes' => array('class' => 'hashtag'))))->toString();
-
-        return $first_delimeter . $hashtag_link;
+    $first_delimeter = $matches[1] ?? '';
+    $hashtag_name = $matches[3] ?? '';
+    $hashtag_tid = $this->hashtagsTids[strtolower($hashtag_name)] ?? '';
+    $hashtag_name = '#' . $hashtag_name;
+    // Hashtag is not exists - show without link.
+    if (empty($hashtag_tid)) {
+      return $first_delimeter . $hashtag_name;
     }
+    // Fatal error: [] operator not supported for strings in
+    // /includes/common.inc on line 2442
+    // Issue comes up when we try to bind attribute to link which has path
+    // parameter of the current page............
+    /*if ($_GET['q'] == 'taxonomy/term/'.$hashtag_tid) {
+    $hashtag_link = l($hashtag_name, 'taxonomy/term/'.$hashtag_tid);
+    } else {
+    $hashtag_link = l($hashtag_name, 'taxonomy/term/'.$hashtag_tid,
+    array('attributes' => array('class' => 'hashtag')));
+    }*/
+    $hashtag_link = Link::fromTextAndUrl($hashtag_name,
+          Url::fromRoute('entity.taxonomy_term.canonical', ['taxonomy_term' => $hashtag_tid],
+              ['attributes' => ['class' => 'hashtag']]))->toString();
+
+    return $first_delimeter . $hashtag_link;
+  }
+
 }
